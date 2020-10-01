5eb2d126232839fbc4ef455c71be151c19c6a147
Aayush Kalia <aayushkalia2000@gmail.comgit config -global user.name Aayush>
2020-10-02 01:22:46 +0530

    JMDgit add -A

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,19 +16,21 @@ class App extends Component {
 
     return (
       <div className='App'>
+      <h1 className='heading'>
+      TAKE BACK YOUR CONTROL !!!
+      </h1>
+      <br />
         <img className='logo' src={logo}></img>
         <br />
-        <div>
-          Welcome to the blockchain...
-        </div>
-        <br />
+
         <div><Link to='/blocks'>Blocks</Link></div>
         <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
         <div><Link to='/transaction-pool'>Transaction Pool</Link></div>
         <br />
         <div className='WalletInfo'>
-          <div>Address: {address}</div>
           <div>Balance: {balance}</div>
+          <div>Address: {address}</div>
+
         </div>
       </div>
     );
